956bdbeb596f36d6fb4b9ec5d5741f0ec0feb804
Joel Baranick <jbaranick@gmail.com>
2024-10-17 17:02:27 -0700

    Publishing sample-app vunspecified

--- app/src/main/java/ai/cheq/sst/android/sample/app/examples/Titled.kt
+++
@@ -1,7 +0,0 @@
-package ai.cheq.sst.android.sample.app.examples
-
-import androidx.annotation.StringRes
-
-interface Titled {
-    fun setTitle(@StringRes titleResource: Int)
-}
--- app/src/main/java/ai/cheq/sst/android/sample/app/examples/sendevent/basic/kotlin/CustomData.kt
+++
@@ -1,7 +0,0 @@
-package ai.cheq.sst.android.sample.app.examples.sendevent.basic.kotlin
-
-class CustomData {
-    val foo = "bar"
-
-    val baz = listOf(1, 2, 3)
-}
